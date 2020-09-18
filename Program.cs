f31b1261f8011c9d8b0d52257f1c975b081f445c
Daniel Burress <thedefside@msn.com>
2020-09-18 11:41:44 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ESLauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dir = System.IO.Directory.GetCurrentDirectory();
+            Environment.SetEnvironmentVariable("HOME", dir);
+            Console.WriteLine($"Setting HOME environment varible to <{Environment.GetEnvironmentVariable("HOME")}>...");
+            var processName = "emultationstation.exe";
+            Process process = new Process();
+            process.StartInfo.FileName = "emulationstation.exe";
+            process.StartInfo.UseShellExecute = true;
+            try
+            {
+                Console.WriteLine($"Starting <{processName}>...");
+                process.Start();
+                Console.WriteLine($"Waiting for <{processName}> to exit...");
+                process.WaitForExit();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
